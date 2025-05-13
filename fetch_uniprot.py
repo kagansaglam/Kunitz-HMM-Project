c703c3560436872d58ddd52d179b520eaddbcec7
kagansaglam <kagansaglam@gmail.com>
2025-05-13 22:35:15 +0200

    Update fetch_uniprot.py

--- fetch_uniprot.py
+++ fetch_uniprot.py
@@ -1,18 +1,18 @@
 import requests
 
-# Define your UniProt IDs
+
 uniprot_ids = [
     "P11111", "P22222", "P12345", "P33333",
     "P54321", "P44444", "P99999", "P55555"
 ]
 
-# Output FASTA file
+
 output_file = "all_proteins.fasta"
 
-# UniProt endpoint
+
 base_url = "https://rest.uniprot.org/uniprotkb/{}.fasta"
 
-# Fetch and write sequences
+
 with open(output_file, "w") as out_f:
     for uid in uniprot_ids:
         response = requests.get(base_url.format(uid))
